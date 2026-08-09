d623a938e0423e27a25cf05bad5872f59578c14b
Strakic <aldin_aganovic1@hotmail.com>
2026-08-09 22:44:47 +1000

    Add sprites, Bin class, and WasteType

--- src/main/java/com/trashslammers/model/TrashItem.java
+++ src/main/java/com/trashslammers/model/TrashItem.java
@@ -1,4 +1,37 @@
 package com.trashslammers.model;
 
 public class TrashItem {
+
+    public enum WasteType {
+        RECYCLING("recycling.png"), // recycling bin
+        CHEMICAL("chemical_waste.png"), //chemical waste bin
+        GENERAL("general_waste.png"), //general waste bin
+        GREEN("green_waste.png"); // green waste bin
+
+        private final String fileName;
+
+        WasteType(String fileName) {
+            this.fileName = fileName;
+        }
+
+        public String getFileName() {
+            return fileName;
+        }
+    }
+
+    private final String name;
+    private final WasteType correctBin;
+
+    public TrashItem(String name, WasteType correctBin) {
+        this.name = name;
+        this.correctBin = correctBin;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public WasteType getCorrectBin() {
+        return correctBin;
+    }
 }
