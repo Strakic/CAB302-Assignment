b480062b5ea8ae56cdbe82a044c015813324fe99
Strakic <aldin_aganovic1@hotmail.com>
2026-08-10 18:39:46 +1000

    Add a bit of login functionality but hasnt been fully implemented

--- src/main/java/com/trashslammers/service/IAuthenticationService.java
+++ src/main/java/com/trashslammers/service/IAuthenticationService.java
@@ -1,10 +1,11 @@
 package com.trashslammers.service;
 
+import com.trashslammers.model.LoginStatus;
 import com.trashslammers.model.User;
 
 public interface IAuthenticationService {
     User signUp(String username, String password);
-    void logIn(String username, String password);
+    LoginStatus logIn(String username, String password);
 }
 // USER BRYN
 // USER ARRAN
