2e6af4e7dc89d409289ecd41f860dbf14c083070
Aldin <n11275723@qut.edu.au>
2026-08-24 13:01:34 +1000

    "added draggable functionality for objects and made the login screen be the same size as main menu"

--- src/main/java/com/trashslammers/controller/GameController.java
+++ src/main/java/com/trashslammers/controller/GameController.java
@@ -1,4 +1,24 @@
 package com.trashslammers.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+
+import java.awt.*;
+import java.net.URL;
+import java.util.ResourceBundle;
+
 public class GameController {
+    @FXML
+            private AnchorPane anchorPane;
+    @FXML
+            private Rectangle rectangle;
+
+    DraggableMaker draggableMaker = new DraggableMaker();
+
+    @Override
+    public void App(URL url, ResourceBundle resourceBundle) {
+        draggableMaker.makeDraggable(rectangle);
+        draggableMaker.makeDraggable(anchorPane);
+    }
+
 }
