c2eea651892e5624279174852c59d9058738c3e7
Azocado <arranabraham@yahoo.com.au>
2026-09-03 14:22:57 +1000

    signup controller added (no code yet)

---
+++ src/main/java/com/trashslammers/controller/SignupController.java
@@ -0,0 +1,19 @@
+package com.trashslammers.controller;
+
+import com.trashslammers.service.AuthenticationService;
+import com.trashslammers.service.IAuthenticationService;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+import java.io.IOException;
+import java.net.URL;
+
+public class SignupController {
+}
