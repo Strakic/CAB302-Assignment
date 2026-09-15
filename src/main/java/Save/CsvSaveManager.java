559c8c77010aac2b7bca535e7a60f395f5a65359
XavierMelville-Git <xaviermelville06@gmail.com>
2026-09-15 20:07:46 +1000

    CsvSaveManager edits

--- src/main/java/Save/CsvSaveManager.java
+++ src/main/java/Save/CsvSaveManager.java
@@ -1,4 +1,14 @@
 package Save;
 
+
 public class CsvSaveManager {
+
+    private static final String HEADER = "type,key,value";
+    private static final int FORMAT_VERSION = 1;
+
+    private final Path saveDir;
+
+    public CsvSaveManager() {
+        this(Paths.get(System.getProperty("user.home"), ".trashslammers", "Save"));
+    }
 }
